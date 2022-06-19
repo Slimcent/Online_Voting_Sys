6489915940476b39ae03233b9580370ff3ab9a63
Achara Obinna Vincent <33299753+Slimcent@users.noreply.github.com>
2022-06-19 21:16:30 +0100

    Email Model

---
+++ OnlineVoting.Models/Dtos/Request/EmailRequestDto.cs
@@ -0,0 +1,11 @@
+﻿namespace OnlineVoting.Models.Dtos.Request
+{
+    public class EmailRequestDto
+    {
+        public string? ToEmail { get; set; }
+        public string? FromEmail { get; set; }
+        public string? ToName { get; set; }
+        public string? FromName { get; set; }
+        public string? AppUrl { get; set; }
+    }
+}
---
+++ OnlineVoting.Services/Infrastructures/SmtpSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace OnlineVoting.Services.Infrastructures
+{
+    public class SmtpSettings
+    {
+        public string? Server { get; set; }
+        public int Port { get; set; }
+        public string? AppUrl { get; set; }
+        public string? SenderName { get; set; }
+        public string? SenderEmail { get; set; }
+        public string? Password { get; set; }
+    }
+}
