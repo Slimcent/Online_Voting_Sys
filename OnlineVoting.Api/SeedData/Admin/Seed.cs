c04c28afa32fc26f14048a6c70270a1de02f9cb2
Slimcent <obinna.achara@outlook.com>
2026-07-17 22:47:51 +0200

    Seeded data to include role and user role claims.

--- OnlineVoting.Api/SeedData/Admin/Seed.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace OnlineVoting.Api.SeedData.Model
-{
-    public class Seed
-    {
-        public IEnumerable<string>? Roles { get; set; }
-
-        public AdminUser? AdminUser { get; set; }
-    }
-}
---
+++ VotingSystem.Data/SeedData/Seed.cs
@@ -0,0 +1,26 @@
+﻿namespace VotingSystem.Data.SeedData
+{
+    public class AdminUser
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public string Role { get; set; }
+        public string Password { get; set; }
+        public int UserTypeId { get; set; }
+    }
+
+    public class SeedUserType
+    {
+        public string Name { get; set; }
+    }
+
+    public class Seed
+    {
+        public AdminUser AdminUser { get; set; }
+        public IEnumerable<string> Roles { get; set; }
+        public IEnumerable<string> RoleClaims { get; set; }
+        public IEnumerable<SeedUserType> UserTypes { get; set; }
+    }
+}
