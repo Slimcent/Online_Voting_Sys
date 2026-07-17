4a5b8b32e46cdab37ba3c43addbd701b1256c750
Slimcent <obinna.achara@outlook.com>
2026-07-17 23:15:38 +0200

    Refactored authorization into dedicated infrastructure module.

--- OnlineVoting.Services/Infrastructures/Jwt/JwtSettings.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace OnlineVoting.Services.Infrastructures.Jwt
-{
-    public class JwtSettings
-    {
-        public string? Secret { get; set; }
-        public string? Expires { get; set; }
-    }
-}
