95c533b99daaaf5f983362cc42161ae1c9236551
Slimcent <obinna.achara@outlook.com>
2026-07-20 00:38:23 +0200

    Added active property to entities.

--- OnlineVoting.Models/Entities/Role.cs
+++ OnlineVoting.Models/Entities/Role.cs
@@ -5,12 +5,7 @@ namespace OnlineVoting.Models.Entities
 {
     public class Role : IdentityRole, ITracker
     {
-        public Role()
-        {
-            IsActive = true;
-        }
-
-        public bool IsActive { get; set; }
+        public bool Active { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
         public string? CreatedBy { get; set; }
--- OnlineVoting.Models/Entities/UserType.cs
+++ OnlineVoting.Models/Entities/UserType.cs
@@ -5,9 +5,7 @@ namespace OnlineVoting.Models.Entities
     public class UserType : ITracker
     {
         public int Id { get; set; }
-
         public string Name { get; set; } = string.Empty;
-
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
         public string? CreatedBy { get; set; }
