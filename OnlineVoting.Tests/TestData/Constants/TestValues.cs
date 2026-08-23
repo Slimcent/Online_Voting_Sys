06efd05e90fa6a12308d531d5676e493c0ae10a8
Slimcent <obinna.achara@outlook.com>
2026-08-23 15:29:43 +0200

    Added initial unit tests and test data structure.

---
+++ OnlineVoting.Tests/TestData/Constants/TestValues.cs
@@ -0,0 +1,9 @@
+﻿namespace OnlineVoting.Tests.TestData.Constants
+{
+    public static class TestValues
+    {
+        public const string ValidEmail = "user@example.com";
+        public const string InvalidEmail = "invalid-email";
+        public const string ValidPassword = "Password123!";
+    }
+}
---
+++ OnlineVoting.Tests/UnitTests/Models/Validators/Request/LoginRequestValidatorTests.cs
@@ -0,0 +1,44 @@
+﻿using FluentValidation.TestHelper;
+using OnlineVoting.Models.Dtos.Request;
+using OnlineVoting.Models.Validators.Request;
+using OnlineVoting.Tests.TestData.Factories;
+
+namespace OnlineVoting.Tests.UnitTests.Models.Validators.Request
+{
+    public class LoginRequestValidatorTests
+    {
+        private readonly LoginRequestValidator _validator = new();
+
+        [Fact]
+        public async Task Validate_ValidRequest_ShouldNotHaveValidationErrors()
+        {
+            LoginRequest request = LoginRequestFactory.CreateValid();
+
+            TestValidationResult<LoginRequest> result = await _validator.TestValidateAsync(request);
+
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+
+        [Fact]
+        public async Task Validate_EmptyPassword_ShouldHavePasswordValidationError()
+        {
+            LoginRequest request = LoginRequestFactory.CreateValid();
+            request.Password = string.Empty;
+
+            TestValidationResult<LoginRequest> result = await _validator.TestValidateAsync(request);
+
+            result.ShouldHaveValidationErrorFor(value => value.Password).WithErrorMessage("Password cannot be empty.");
+        }
+
+        [Fact]
+        public async Task Validate_InvalidEmail_ShouldHaveEmailValidationError()
+        {
+            LoginRequest request = LoginRequestFactory.CreateValid();
+            request.Email = "invalid-email";
+
+            TestValidationResult<LoginRequest> result = await _validator.TestValidateAsync(request);
+
+            result.ShouldHaveValidationErrorFor(value => value.Email);
+        }
+    }
+}
